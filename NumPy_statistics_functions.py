62c606f7175acdbc7a706dbe266616a624478515
declan-1992 <55177948+declan-1992@users.noreply.github.com>
2019-10-10 16:10:28 +0100

    Update NumPy_statistics_functions.py

--- NumPy_statistics_functions.py
+++ NumPy_statistics_functions.py
@@ -2,14 +2,14 @@ import numpy as np
 
 rainfall = np.array([5.21, 3.76, 3.27, 2.35, 1.89, 1.55, 0.65, 1.06, 1.72, 3.35, 4.82, 5.11])
 
-rain_mean = np.mean(rainfall)
+rain_mean = np.mean(rainfall)   #mean
 
-rain_median = np.median(rainfall)
+rain_median = np.median(rainfall)   #median
 
-first_quarter = np.percentile(rainfall, 25)
+first_quarter = np.percentile(rainfall, 25)    #lower quartile
 
-third_quarter = np.percentile(rainfall, 75)
+third_quarter = np.percentile(rainfall, 75)    #upper quartile
 
-interquartile_range = third_quarter - first_quarter 
+interquartile_range = third_quarter - first_quarter     #interquartile range
 
-rain_std = np.std(rainfall)
+rain_std = np.std(rainfall)    #standard deviation 
